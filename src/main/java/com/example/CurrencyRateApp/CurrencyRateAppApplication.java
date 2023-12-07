268cb3c434bef943bb53a4dae32c5e41f195135d
Robert Salicki <robsal12@gmail.com>
2023-12-07 12:19:38 +0100

    01-INITIAL PROJECT

---
+++ src/main/java/com/example/CurrencyRateApp/CurrencyRateAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.CurrencyRateApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyRateAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CurrencyRateApp/CurrencyRateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CurrencyRateApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
