5e6926d75652e5deca224c9e68dcce5da26c531e
Robert Salicki <robsal12@gmail.com>
2023-12-12 12:59:12 +0100

    05-Controller-exceptions-handling

--- src/main/java/com/example/currency/service/CurrencyRateService.java
+++ src/main/java/com/example/currency/service/CurrencyRateService.java
@@ -5,7 +5,6 @@ import com.example.currency.webclient.CurrencyClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
