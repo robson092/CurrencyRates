fbeac961d39725f99370133e68524abbb7e96abe
Robert Salicki <robsal12@gmail.com>
2023-12-12 16:21:21 +0100

    06-Adding-methods-to-webclient-for-additional-endpoints

--- src/main/java/com/example/currency/controller/CurrencyRateController.java
+++ src/main/java/com/example/currency/controller/CurrencyRateController.java
@@ -6,15 +6,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/rates")
 public class CurrencyRateController {
 
     private final CurrencyRateService currencyRateService;
 
-    @GetMapping("/rates/c/{currencyCode}")
+    @GetMapping("/{currencyCode}")
     public ResponseEntity<CurrencyRateDto> getCurrency(@PathVariable String currencyCode) {
         return currencyRateService.getCurrencyRate(currencyCode)
                 .map(ResponseEntity::ok)
--- src/main/java/com/example/currency/webclient/CurrencyClient.java
+++ src/main/java/com/example/currency/webclient/CurrencyClient.java
@@ -1,7 +1,6 @@
 package com.example.currency.webclient;
 
 import com.example.currency.dto.NbpCurrencyDto;
-import com.example.currency.dto.NbpCurrencyRatesDto;
 import com.example.currency.model.CurrencyRateDto;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -9,16 +8,15 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class CurrencyClient {
 
-    private static final String CURRENCY_URL = "http://api.nbp.pl/api/exchangerates";
+    private static final String CURRENCY_URL = "http://api.nbp.pl/api/exchangerates/rates/c";
     private final RestTemplate restTemplate = new RestTemplate();
 
     private <T> T callGetMethod(String url, Class<T> responseType, Object... objects) {
         return restTemplate.getForObject(CURRENCY_URL + url, responseType, objects);
     }
 
-    public CurrencyRateDto getRateForGivenCurrency(String table, String currencyCode) {
-        NbpCurrencyDto nbpCurrencyDto = callGetMethod("/rates/{table}/{code}", NbpCurrencyDto.class,
-                table, currencyCode);
+    public CurrencyRateDto getRateForGivenCurrency(String currencyCode) {
+        NbpCurrencyDto nbpCurrencyDto = callGetMethod("/{code}", NbpCurrencyDto.class, currencyCode);
         return CurrencyRateDto.builder()
                 .currency(nbpCurrencyDto.getCurrency())
                 .code(nbpCurrencyDto.getCode())
@@ -28,22 +26,52 @@ public class CurrencyClient {
                 .build();
     }
 
-//    public Optional<String> getRateForGivenCurrencyInGivenDay(String table, String currencyCode, String date) {
-//        return callGetMethod(CURRENCY_URL + "/rates/{table}/{code}/{date}", String.class, table, currencyCode, date);
-//    }
+    public CurrencyRateDto getRateForGivenCurrencyInGivenDay(String currencyCode, String date) {
+        NbpCurrencyDto nbpCurrencyDto = callGetMethod(CURRENCY_URL + "/{code}/{date}", NbpCurrencyDto.class,
+                currencyCode, date);
+        return CurrencyRateDto.builder()
+                .currency(nbpCurrencyDto.getCurrency())
+                .code(nbpCurrencyDto.getCode())
+                .effectiveDate(nbpCurrencyDto.getRates().get(0).getEffectiveDate())
+                .sell(nbpCurrencyDto.getRates().get(0).getBid())
+                .buy(nbpCurrencyDto.getRates().get(0).getAsk())
+                .build();
+    }
+
+    public CurrencyRateDto getTodayRateForGivenCurrency(String currencyCode) {
+        NbpCurrencyDto nbpCurrencyDto = callGetMethod(CURRENCY_URL + "/{code}/", NbpCurrencyDto.class,
+                currencyCode, "today");
+        return CurrencyRateDto.builder()
+                .currency(nbpCurrencyDto.getCurrency())
+                .code(nbpCurrencyDto.getCode())
+                .effectiveDate(nbpCurrencyDto.getRates().get(0).getEffectiveDate())
+                .sell(nbpCurrencyDto.getRates().get(0).getBid())
+                .buy(nbpCurrencyDto.getRates().get(0).getAsk())
+                .build();
+    }
 //
-//    public Optional<String> getTodayRateForGivenCurrency(String table, String currencyCode) {
-//        return callGetMethod(CURRENCY_URL + "/rates/{table}/{code}/", String.class, table, currencyCode, "today");
-//    }
+    public CurrencyRateDto getRateForGivenCurrencyInTimeRange(String currencyCode,
+                                                               String startDate, String endDate) {
+        NbpCurrencyDto nbpCurrencyDto = callGetMethod(CURRENCY_URL + "/{code}/{date}", NbpCurrencyDto.class, currencyCode,
+                startDate, endDate);
+        return CurrencyRateDto.builder()
+                .currency(nbpCurrencyDto.getCurrency())
+                .code(nbpCurrencyDto.getCode())
+                .effectiveDate(nbpCurrencyDto.getRates().get(0).getEffectiveDate())
+                .sell(nbpCurrencyDto.getRates().get(0).getBid())
+                .buy(nbpCurrencyDto.getRates().get(0).getAsk())
+                .build();
+    }
 //
-//    public Optional<String> getRateForGivenCurrencyInTimeRange(String table, String currencyCode,
-//                                                               String startDate, String endDate) {
-//        return callGetMethod(CURRENCY_URL + "/rates/{table}/{code}/{date}", String.class, table, currencyCode,
-//                startDate, endDate);
-//    }
-//
-//    public Optional<String> getLastDaysRateForGivenCurrency(String table, String currencyCode, int topCount) {
-//        return callGetMethod(CURRENCY_URL + "/rates/{table}/{code}/{topCount}/", String.class, table, currencyCode,
-//                "last", topCount);
-//    }
+    public CurrencyRateDto getLastDaysRateForGivenCurrency(String currencyCode, int topCount) {
+        NbpCurrencyDto nbpCurrencyDto = callGetMethod(CURRENCY_URL + "/{code}/{topCount}/", NbpCurrencyDto.class, currencyCode,
+                "last", topCount);
+        return CurrencyRateDto.builder()
+                .currency(nbpCurrencyDto.getCurrency())
+                .code(nbpCurrencyDto.getCode())
+                .effectiveDate(nbpCurrencyDto.getRates().get(0).getEffectiveDate())
+                .sell(nbpCurrencyDto.getRates().get(0).getBid())
+                .buy(nbpCurrencyDto.getRates().get(0).getAsk())
+                .build();
+    }
 }
