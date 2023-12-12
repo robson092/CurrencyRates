8ea1a33dfe621f6be7fdcf7aef12c08ee5fa1d9f
Robert Salicki <robsal12@gmail.com>
2023-12-12 16:32:58 +0100

    07-Adding-methods-to-webclient-for-additional-endpoints

--- src/main/java/com/example/currency/service/CurrencyRateService.java
+++ src/main/java/com/example/currency/service/CurrencyRateService.java
@@ -19,5 +19,19 @@ public class CurrencyRateService {
         return Optional.ofNullable(currencyClient.getRateForGivenCurrency(currencyCode));
     }
 
+    public Optional<CurrencyRateDto> getCurrencyRateForGivenDay(String currencyCode, String date) {
+        return Optional.ofNullable(currencyClient.getRateForGivenCurrencyInGivenDay(currencyCode, date));
+    }
 
+    public Optional<CurrencyRateDto> getCurrencyRateForToday(String currencyCode) {
+        return Optional.ofNullable(currencyClient.getTodayRateForGivenCurrency(currencyCode));
+    }
+
+    public Optional<CurrencyRateDto> getCurrencyRateForTimeRange(String currencyCode, String startDate, String endDate) {
+        return Optional.ofNullable(currencyClient.getRateForGivenCurrencyInTimeRange(currencyCode, startDate, endDate));
+    }
+
+    public Optional<CurrencyRateDto> getLastCountOfCurrencyRate(String currencyCode, int topCount) {
+        return Optional.ofNullable(currencyClient.getLastDaysRateForGivenCurrency(currencyCode, topCount));
+    }
 }
